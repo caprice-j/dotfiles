ee1a0bea01a6cd78de0255f582fb7e05b2efe7d5
caprice-j <skyfly0008@gmail.com>
2016-01-10 02:08:06 +0900

    make .tmux.conf visible

--- symboliclink.sh
+++ symboliclink.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
-ln -s init.el ~/.emacs.d/init.el
-ln -s .tmux.conf ~/.tmux.conf
+TGT_EMACS_FILE=~/.emacs.d/init.el
+TGT_TMUX_FILE=~/.tmux.conf
+
+DOTFILES=~/dotfiles
+
+rm -f  ~/.emacs
+rm -f  ~/.emacs.el
+rm -f  ${TGT_EMACS_FILE}
+ln -s  ${DOTFILES}/init.el     ${TGT_EMACS_FILE}
+rm -f  ${TGT_TMUX_FILE}
+ln -s  ${DOTFILES}/tmux.conf   ${TGT_TMUX_FILE}
