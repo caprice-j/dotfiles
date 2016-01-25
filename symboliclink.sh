a8934b159e91a5e4b2706f990782fc95ab8c4ee0
caprice-j <skyfly0008@gmail.com>
2016-01-25 19:07:48 +0900

    add skt

--- symboliclink.sh
+++ symboliclink.sh
@@ -3,7 +3,8 @@
 TGT_EMACS_FILE=~/.emacs.d/init.el
 TGT_EMACS_INIT_MODELINE_FILE=~/.emacs.d/init-modeline.el
 TGT_TMUX_FILE=~/.tmux.conf
-TGT_TMUXINATOR=~/.tmuxinator/res.yml
+TGT_TMUXINATOR1=~/.tmuxinator/res.yml
+TGT_TMUXINATOR2=~/.tmuxinator/skt.yml
 
 DOTFILES=~/dotfiles
 
@@ -19,4 +20,5 @@ ln -fs ${DOTFILES}/init-modeline.el ${TGT_EMACS_INIT_MODELINE_FILE}
 ln -fs  ${DOTFILES}/tmux.conf   ${TGT_TMUX_FILE}
 
 # mux start res
-ln -fs  ${DOTFILES}/res.yml     ${TGT_TMUXINATOR}
+ln -fs  ${DOTFILES}/res.yml     ${TGT_TMUXINATOR1}
+ln -fs  ${DOTFILES}/skt.yml     ${TGT_TMUXINATOR2}
